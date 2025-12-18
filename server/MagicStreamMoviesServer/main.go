a0e26e8da6b7dd79ecf6cebaf6b3e705216894a5
Santhosh785 <sandysanthosh785@gmail.com>
2025-12-18 15:15:21 +0530

    Initial commit

---
+++ server/MagicStreamMoviesServer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStream")
+	})
+
+	if err := router.Run(":8081"); err != nil {
+		fmt.Print("failed to start", err)
+	}
+}
